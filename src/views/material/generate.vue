f20e103165d38be46cb63a6d013efa76e04fedaa
wwd <37473983+mmzwwd@users.noreply.github.com>
2021-09-02 14:01:10 +0800

    Update generate.vue

--- src/views/material/generate.vue
+++ src/views/material/generate.vue
@@ -92,7 +92,7 @@
             fit="scale-down"
             style="width: 400px; height: 400px;"
             :src="imgUrl"
-            :preview-src-list="srcList"
+            :preview-src-list="srcLists"
           ></el-image>
         </div>
       </el-col>
@@ -118,7 +118,10 @@ export default {
       },
       imgUrl:
         "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
-      srcList: [this.imgUrl]
+      srcLists: ['https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg',
+      "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+          'https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg',]
+          
     };
   },
   created: function() {
